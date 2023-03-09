affd77243c4ff87c10b8b257d8c71c1a4b040893
Jakub Kordella <kubusmateusz1@gmail.com>
2023-03-09 11:11:22 +0100

    Update 3.0

--- 09032023.py
+++ 09032023.py
@@ -1,5 +1,20 @@
 import PySimpleGUI as gui
+
+# KLASY
+class TODOTask:
+    def __init__(self, id, title, description, done):
+        self.Id = id
+        self.Title = title
+        self.Description = description
+        self.Done = done
+
+
+
 gui.theme('DarkGrey4')
+# GLOBALNE
+taskNo = 1
+tasksList = []
+titlesList = []
 
 inputs_layout = [
     [gui.Text('Dodawanie zadania', font=('Comic Sans MS', 16))],
@@ -8,7 +23,10 @@ inputs_layout = [
 ]
 
 buttons_layout = [
-    [gui.Button('Dodaj', key='add', size=(10, 1)), gui.Button('Zamknij', key='close', size=(10, 1))]
+    [
+        gui.Button('Dodaj', key='add', size=(10, 1)),
+        gui.Button('Zamknij', key='close', size=(10, 1))
+    ]
 ]
 
 output_layout = [
@@ -36,5 +54,19 @@ table = [
 
 layout = inputs_layout + buttons_layout + output_layout + headers + table
 
-okno = gui.Window('TODO List', layout, font=('Comic Sans MS', 12), element_justification='center')
-okno.read()
+okno = gui.Window('TODO List', layout, font=('Comic Sans MS', 12), element_justification='center', element_padding=(10, 5), no_titlebar=True, grab_anywhere=True)
+
+# FUNKCJE
+def updateListsAndCombo():
+    tasksList.append(task)
+    titlesList.append(task.Title)
+    okno['titles_combo'].update(values=titlesList, value=len(titlesList) - 1)
+
+while True:
+    event, values = okno.read()
+    if event == "add":
+        task = TODOTask(taskNo, values['inp_title'], values['inp_description'], False)
+        taskNo += 1
+        updateListsAndCombo()
+    if event == "close":
+        break
